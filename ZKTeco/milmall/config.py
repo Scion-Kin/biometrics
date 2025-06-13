b052fc097b5cf7feaa06912f36d99c5317a0e7d3
Mugabo Gusenga <116818653+Scion-Kin@users.noreply.github.com>
2025-06-13 16:42:25 +0200

    Update config.py

--- ZKTeco/milmall/config.py
+++ ZKTeco/milmall/config.py
@@ -6,7 +6,7 @@ urls = {
 }
 business_id = 1  # Change this to your business ID
 api_url = "https://milmall.rw" # Change this to your MilMall API URL
-client_id = "7"  # Change this to your MilMall client ID
-client_secret = "wS5THVP6f0Qy8w6kRmulqnkUvtv8jPGQzSrorFyv"  # Change this to your MilMall client secret
-username = "gilmed_ndoli"  # Change this to your MilMall username
-password = "ndoli2024"  # Change this to your MilMall password
+client_id = "0"  # Change this to your MilMall client ID
+client_secret = "fsh"  # Change this to your MilMall client secret
+username = "t"  # Change this to your MilMall username
+password = "0"  # Change this to your MilMall password
