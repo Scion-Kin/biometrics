a02e59fc2b69bada2cdf8fbf8bbad5a9372aec38
Mugabo Gusenga <dinturner17@gmail.com>
2025-06-13 16:15:19 +0200

    Minor fix

--- cf/runner.sh
+++ cf/runner.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+source venv/bin/activate
+
 cd ZKTeco
-./puller.py --verbose
-./main.py -m MilMall --verbose
-./update_checker.sh
+python3 puller.py --verbose
+python3 main.py -m MilMall --verbose
+
+cd ..
+cf/update_checker.sh
