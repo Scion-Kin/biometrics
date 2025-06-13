eac715f5d89e21e171ff7f10fb7ed6e8d305cfb2
Mugabo Gusenga <dinturner17@gmail.com>
2025-06-13 17:02:02 +0200

    Remove verbose

--- cf/runner.sh
+++ cf/runner.sh
@@ -3,8 +3,8 @@
 source venv/bin/activate
 
 cd ZKTeco
-python3 puller.py --verbose
-python3 main.py -m MilMall --verbose
+python3 puller.py
+python3 main.py -m MilMall
 
 cd ..
 ./cf/update_checker.sh
