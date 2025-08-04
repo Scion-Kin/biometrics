c7ec08b2e036e43b0dc36b0398171ce4be981b86
Mugabo Gusenga <dinturner17@gmail.com>
2025-08-04 15:52:55 +0200

    Add central logging

--- ZKTeco/logger.py
+++ ZKTeco/logger.py
@@ -1,4 +1,13 @@
 from logmachine import ContribLog
 import os, sys
 
-logger = ContribLog("", debug_level=os.getenv('DEBUG_LEVEL', 0), verbose="--verbose" in sys.argv)
+logger = ContribLog(
+    "",
+    debug_level=os.getenv('DEBUG_LEVEL', 0),
+    verbose="--verbose" in sys.argv,
+    central={
+        "url": "https://logmachine.bufferpunk.com",
+        "room": "bio-zk-monitoring",
+        "headers": {}
+    },
+)
