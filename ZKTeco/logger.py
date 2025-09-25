3498c15169785d64790061763a3d2d072c08ce52
Mugabo Gusenga <dinturner17@gmail.com>
2025-09-25 16:15:37 +0200

    Remove unnecessary central logging

--- ZKTeco/logger.py
+++ ZKTeco/logger.py
@@ -5,12 +5,12 @@ logger = ContribLog(
     "",
     debug_level=os.getenv('DEBUG_LEVEL', 0),
     verbose="--verbose" in sys.argv,
-    central={
-        "url": "https://logmachine.bufferpunk.com",
-        "room": "bio-zk-monitoring",
-        "headers": {}
-    },
-    socketio=True
+#    central={
+#        "url": "https://logmachine.bufferpunk.com",
+#        "room": "bio-zk-monitoring",
+#        "headers": {}
+#    },
+#   socketio=True
 )
 
 logging.getLogger("pymongo.client").setLevel(0)
